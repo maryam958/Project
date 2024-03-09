77239d229c74e500d4673457a32d1e1108435f85
maryam958 <72671890+maryam958@users.noreply.github.com>
2024-03-09 23:51:57 +0200

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { globalError } from "./src/services/asyncHandler.js";
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT
 const baseUrl = process.env.BASEURL
 
 app.use(express.json());
