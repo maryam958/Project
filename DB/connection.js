e872e2e7604f5321727851dcc56da2db7ec3f745
maryam958 <72671890+maryam958@users.noreply.github.com>
2024-03-10 15:55:09 +0200

    Deploy

--- DB/connection.js
+++ DB/connection.js
@@ -1,10 +1,14 @@
 import mysql from 'mysql2'
 
 const connectDB = mysql.createConnection({
-    host:'127.0.0.1',
-    database:'mytask',
-    user:'root',
-    password:''
+    // host:'127.0.0.1',
+    // database:'mytask',
+    // user:'root',
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+
 
 
 })
